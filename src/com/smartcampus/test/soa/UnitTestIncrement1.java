a5974576b7c713ace6cc173ea9f743d953c26e4d
Andrea De Vita <andrea.devita@gmail.com>
2014-07-07 16:51:37 +0200

    Unit Test Increment1 - ACC

--- src/com/smartcampus/test/soa/UnitTestIncrement1.java
+++ src/com/smartcampus/test/soa/UnitTestIncrement1.java
@@ -1,38 +1,59 @@
 package com.smartcampus.test.soa;
 
-import java.text.DecimalFormat;
 
 import javax.xml.bind.JAXBElement;
 
+import com.smartcampus.acc.ArtificialClimateControl;
 import com.smartcampus.acc.ArtificialClimateControlPortType;
 import com.smartcampus.acc.xsd.IndoorStatus;
+import com.smartcampus.luminancemanagement.LuminanceManagementPortType;
+
 import com.smartcampus.naturalclimatesystem.NaturalClimateSystem;
 import com.smartcampus.naturalclimatesystem.NaturalClimateSystemPortType;
 import com.smartcampus.naturalclimatesystem.xsd.Location;
 import com.smartcampus.naturalclimatesystem.xsd.ObjectFactory;
 import com.smartcampus.naturalclimatesystem.xsd.WeatherCondition;
+
 import com.smartcampus.paths.PathsPortType;
 import com.smartcampus.paths.xsd.PathComponent;
 import com.smartcampus.paths.xsd.PathData;
 import com.smartcampus.roomusagedatabase.RoomUsageDatabasePortType;
 import com.smartcampus.roomusagedatabase.xsd.EventData;
+import com.smartcampus.servicesfacilities.ServicesAndFacilitiesPortType;
 
 
 public class UnitTestIncrement1 {
-	static NaturalClimateSystemPortType s;
+	private static ArtificialClimateControlPortType acc;
+	private static NaturalClimateSystemPortType nc;
+	private static PathsPortType p;
+	private static RoomUsageDatabasePortType rud;
+	private static LuminanceManagementPortType lm;
+	private static ServicesAndFacilitiesPortType sf;
 
 	public static double getPositiveRate(int err, int tot) {
-		
 		return 100 - (((double)err) / ((double)tot))*100;
 	} 
 
+	
+	public static void printSteps(int i, int c){
+		if (i==1) 
+			System.out.print("Round: " + i + "/" + c);
+		else if (i<c)
+			System.out.print(".");
+		else
+			System.out.println(" " + i + "/" + c);
+	}
+	
 	public static void testNCS(int c) {
 		int weatherConditionError = 0;
 		int openWindowError = 0;
 		int closeWindowError = 0;
-
+		
+		nc = new NaturalClimateSystem().getNaturalClimateSystemHttpSoap11Endpoint();
+		
 		for (int i=1; i<=c; i++) {
-			s = new NaturalClimateSystem().getNaturalClimateSystemHttpSoap11Endpoint();
+			printSteps(i, c);
+			
 			ObjectFactory o = new ObjectFactory();
 			Location l = new Location();
 			String rid = "A12";
@@ -42,14 +63,14 @@ public class UnitTestIncrement1 {
 			l.setRoomId(roomId);
 			l.setWindowsId(wid);
 			
-			System.out.println("Cicle " + i + "/" + c);
 			
-			if (s.openWindow(l) == false)
+			
+			if (nc.openWindow(l) == false)
 				openWindowError ++;
-			if (s.closeWindow(l) == false)
+			if (nc.closeWindow(l) == false)
 				closeWindowError ++;
 	
-			WeatherCondition we = s.getWeatherCondition(l);
+			WeatherCondition we = nc.getWeatherCondition(l);
 			if (we == null) {
 				weatherConditionError ++;
 			}
@@ -64,16 +85,39 @@ public class UnitTestIncrement1 {
 		totAvgRate += getPositiveRate(weatherConditionError,c);
 		countAvgRate++;
 
-		System.out.println("Unit Test - WeatherCondition - Report");
-		System.out.println("Tested methods");
-		System.out.println("	openWindow()");
-		System.out.println("	closeWindow()");
-		System.out.println("	getWeatherCondition()");
+		
+		System.out.println("Tested methods:  openWindow(); closeWindow(); getWeatherCondition()");
 		System.out.println("Average positive rate \t" + totAvgRate / countAvgRate  + "%");
 	}
 
+	public static void testACC(int c) {
+		int indoorStatusError = 0;
+		acc = new ArtificialClimateControl().getArtificialClimateControlHttpSoap11Endpoint();
+		
+		for (int i=1; i<=c; i++) {
+			printSteps(i, c);
+			IndoorStatus status = acc.getIndoorStatus("ADInform1");
+			
+			if (status == null) {
+				indoorStatusError++;
+			}
+		}
+		double totAvgRate = 0;
+		int countAvgRate = 0;
+		totAvgRate += getPositiveRate(indoorStatusError,c);
+		countAvgRate++;
+		
+		System.out.println("Tested methods: getIndoorStatus();");
+		System.out.println("Average positive rate \t" + totAvgRate / countAvgRate  + "%");
+	}
+	
+	
 	public static void main(String[] args) {
-		testNCS(10);
+		System.out.println("Unit Test");
+		System.out.println("\n   NaturalClimateSystem - Report");
+		testNCS(100);
+		System.out.println("\n   ArtificialClimateContro - Report");
+		testACC(100);
 
 
 
