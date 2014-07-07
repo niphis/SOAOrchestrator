2924895cb89570808bfafda54d2b20e1d48e5bdb
Tommaso Latini <t.latini@sssup.it>
2014-07-07 16:09:12 +0200

    Integration test main done. TODO. Print statistics

---
+++ src/com/smartcampus/test/soa/IntegrationTestIncremental2.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ */
+package com.smartcampus.test.soa;
+
+import java.util.PriorityQueue;
+
+import com.smartcampus.acc.ArtificialClimateControl;
+import com.smartcampus.acc.ArtificialClimateControlPortType;
+import com.smartcampus.naturalclimatesystem.NaturalClimateSystem;
+import com.smartcampus.naturalclimatesystem.NaturalClimateSystemPortType;
+import com.smartcampus.paths.Paths;
+import com.smartcampus.paths.PathsPortType;
+import com.smartcampus.roomusagedatabase.RoomUsageDatabase;
+import com.smartcampus.roomusagedatabase.RoomUsageDatabasePortType;
+import com.smartcampus.test.soa.Orchestrator_part1.Error;
+import com.smartcampus.test.soa.Orchestrator_part1.TimerEvent;
+import com.smartcampus.test.soa.Orchestrator_part1.WakeReason;
+
+/**
+ * @author Tom
+ *
+ */
+public class IntegrationTestIncremental2 {
+
+	/**
+	 * @param args
+	 */
+	private static PriorityQueue<TimerEvent> timers = new PriorityQueue<TimerEvent>();
+	
+	private static ArtificialClimateControlPortType acc = new ArtificialClimateControl()
+		.getArtificialClimateControlHttpSoap11Endpoint();
+	private static NaturalClimateSystemPortType nc = new NaturalClimateSystem()
+		.getNaturalClimateSystemHttpSoap11Endpoint();
+	private static PathsPortType p = new Paths().getPathsHttpSoap11Endpoint();
+	private static RoomUsageDatabasePortType rud = new RoomUsageDatabase().getRoomUsageDatabaseHttpSoap11Endpoint();
+		
+	public static void testOrchestrator(int maxIterations) {
+		
+		int climateControlErrorCounter = 0;
+		int climateControlEventCounter = 0;
+		int dailyWakeupErrorCounter = 0;
+		int dailyWakeupEventCounter = 0;
+		
+		for (int i=0; i<maxIterations; i++) {
+		
+			// INITIAL EVENT SCHEDULING
+			
+			Error res = Error.NO_EVENT;
+			TimerEvent a = new TimerEvent(WakeReason.DAILY_WAKEUP);
+
+			timers.add(a);
+						
+			Orchestrator_part1.setArtificialClimateControlPortType(acc);
+			Orchestrator_part1.setNaturalClimateSystemPortType(nc);
+			Orchestrator_part1.setPathsPortType(p);
+			Orchestrator_part1.setRoomUsageDatabasePortType(rud);
+			
+			do {
+				
+				WakeReason w = timers.element().reason;
+				
+				// Counter on event
+				
+				switch(w) {
+				case DAILY_WAKEUP:
+					dailyWakeupEventCounter++;
+					break;
+				case CLIMATE_WAKEUP:
+					climateControlEventCounter++;
+					break;
+				}
+				
+				res = Orchestrator_part1.wakeUp(timers);
+				
+				// Counter on error
+				
+				switch (res) {
+				case DAILY_WAKEUP_ERROR:
+					climateControlErrorCounter++;
+					break;
+				case CLIMATE_WAKEUP_ERROR:
+					climateControlErrorCounter++;
+					break;
+				}
+			}
+			
+			while (res != Error.NO_EVENT);
+			
+			// Print statistics
+		}
+	}
+	
+	public static void main(String[] args) {
+		testOrchestrator(1000);
+	}
+}
--- src/com/smartcampus/test/soa/IntegrationTestIncremental4.java
+++ src/com/smartcampus/test/soa/IntegrationTestIncremental4.java
@@ -7,21 +7,25 @@ import java.util.PriorityQueue;
 
 import com.smartcampus.acc.ArtificialClimateControl;
 import com.smartcampus.acc.ArtificialClimateControlPortType;
+import com.smartcampus.luminancemanagement.LuminanceManagement;
+import com.smartcampus.luminancemanagement.LuminanceManagementPortType;
 import com.smartcampus.naturalclimatesystem.NaturalClimateSystem;
 import com.smartcampus.naturalclimatesystem.NaturalClimateSystemPortType;
 import com.smartcampus.paths.Paths;
 import com.smartcampus.paths.PathsPortType;
 import com.smartcampus.roomusagedatabase.RoomUsageDatabase;
 import com.smartcampus.roomusagedatabase.RoomUsageDatabasePortType;
-import com.smartcampus.test.soa.Orchestrator_part1.Error;
-import com.smartcampus.test.soa.Orchestrator_part1.TimerEvent;
-import com.smartcampus.test.soa.Orchestrator_part1.WakeReason;
+import com.smartcampus.servicesfacilities.ServicesAndFacilities;
+import com.smartcampus.servicesfacilities.ServicesAndFacilitiesPortType;
+import com.smartcampus.test.soa.Orchestrator.TimerEvent;
+import com.smartcampus.test.soa.Orchestrator.WakeReason;
+import com.smartcampus.test.soa.Orchestrator.Error;
 
 /**
  * @author Tom
  *
  */
-public class UnitTestIncremental2 {
+public class IntegrationTestIncremental4 {
 
 	/**
 	 * @param args
@@ -34,13 +38,27 @@ private static PriorityQueue<TimerEvent> timers = new PriorityQueue<TimerEvent>(
 		.getNaturalClimateSystemHttpSoap11Endpoint();
 	private static PathsPortType p = new Paths().getPathsHttpSoap11Endpoint();
 	private static RoomUsageDatabasePortType rud = new RoomUsageDatabase().getRoomUsageDatabaseHttpSoap11Endpoint();
-
-	public static void testOrchestrator(int maxIterations) {
+	private static LuminanceManagementPortType lm = new LuminanceManagement()
+		.getLuminanceManagementHttpSoap11Endpoint();
+	private static ServicesAndFacilitiesPortType sf =  new ServicesAndFacilities()
+		.getServicesAndFacilitiesHttpSoap11Endpoint();
+	
+	private static void testOrchestrator(int maxIterations) {
 		
 		int climateControlErrorCounter = 0;
 		int climateControlEventCounter = 0;
+		
 		int dailyWakeupErrorCounter = 0;
 		int dailyWakeupEventCounter = 0;
+		
+		int luminanceManagementErrorCounter = 0;
+		int luminanceManagementEventCounter = 0;
+		
+		int foodWakeupErrorCounter = 0;
+		int foodWakeupEventCounter = 0;
+		
+		int cleanWakeupErrorCounter = 0;
+		int cleanWakeupEventCounter = 0;
 		
 		for (int i=0; i<maxIterations; i++) {
 		
@@ -50,41 +68,69 @@ private static PriorityQueue<TimerEvent> timers = new PriorityQueue<TimerEvent>(
 			TimerEvent a = new TimerEvent(WakeReason.DAILY_WAKEUP);
 
 			timers.add(a);
-						
-			Orchestrator_part1.setArtificialClimateControlPortType(acc);
-			Orchestrator_part1.setNaturalClimateSystemPortType(nc);
-			Orchestrator_part1.setPathsPortType(p);
-			Orchestrator_part1.setRoomUsageDatabasePortType(rud);
+			Orchestrator.setArtificialClimateControlPortType(acc);
+			Orchestrator.setNaturalClimateSystemPortType(nc);
+			Orchestrator.setPathsPortType(p);
+			Orchestrator.setRoomUsageDatabasePortType(rud);
+			Orchestrator.setLuminanceManagementPortType(lm);
+			Orchestrator.setServicesAndFacilitiesPortType(sf);
 			
 			do {
 				
 				WakeReason w = timers.element().reason;
 				// Counter on event
+	
 				switch(w) {
 				case DAILY_WAKEUP:
 					dailyWakeupEventCounter++;
+					break;
 				case CLIMATE_WAKEUP:
 					climateControlEventCounter++;
+					break;
+				case LUMINANCE_WAKEUP:
+					luminanceManagementEventCounter++;
+					break;
+				case FOOD_WAKEUP:
+					foodWakeupEventCounter++;
+					break;
+				case CLEANING_WAKEUP:
+					cleanWakeupEventCounter++;
+					break;
 				}
-				
-				res = Orchestrator_part1.wakeUp(timers);
+	
+				res = Orchestrator.wakeUp(timers);
 				
 				// Counter on error
+				
 				switch (res) {
 				case DAILY_WAKEUP_ERROR:
 					climateControlErrorCounter++;
+					break;
 				case CLIMATE_WAKEUP_ERROR:
 					climateControlErrorCounter++;
+					break;
+				case LUMINANCE_WAKEUP_ERROR:
+					luminanceManagementErrorCounter++;
+					break;
+				case FOOD_WAKEUP_ERROR:
+					foodWakeupErrorCounter++;
+					break;
+				case CLEANING_WAKEUP_ERROR:
+					cleanWakeupErrorCounter++;
+					break;
 				}
 			}
 			
 			while (res != Error.NO_EVENT);
-			
-			
+		
 		}
+		
+		
+		
 	}
 	
 	public static void main(String[] args) {
 		testOrchestrator(1000);
 	}
+
 }
