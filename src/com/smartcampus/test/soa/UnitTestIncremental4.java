ba6927e9346bf6609b589afef199fa86da0b61d9
Tommaso Latini <t.latini@sssup.it>
2014-07-07 15:14:44 +0200

    Skeleton of test main

---
+++ src/com/smartcampus/test/soa/UnitTestIncremental4.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.smartcampus.test.soa;
+
+/**
+ * @author Tom
+ *
+ */
+public class UnitTestIncremental4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
